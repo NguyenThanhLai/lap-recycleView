c11ad79f6f6edaed44260cd928196b6d48dcdcd5
Thanh Lai <lainguyen523@gmail.com>
2019-01-15 13:40:07 +0700

    first commit

---
+++ app/src/main/java/com/example/laipc/app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.laipc.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    RecyclerViewAdapter mRcvAdapter;
+    List<String> data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        data = new ArrayList<>();
+        data.add("Nguyễn Minh Hưng");
+        data.add("Hoàng Minh Lợi");
+        data.add("Nguyễn Duy Bảo");
+        data.add("Nguyễn Ngọc Doanh");
+        data.add("Nguyễn Phạm Thế Hà");
+        data.add("Trần Anh Đức");
+        data.add("Trần Minh Hải");
+        mRcvAdapter = new RecyclerViewAdapter(this,data);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mRcvAdapter);
+
+    }
+}
